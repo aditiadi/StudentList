5dd17342ef163413223ee6d92487f075c05ab4f7
Aditi <aditiadi@sheridancollege.ca>
2022-02-04 18:34:28 -0500

    Added part-time student

---
+++ src/studentlist/PartTimeStudent.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentlist;
+
+/**
+ *
+ * @author Aditi
+ */
+public class PartTimeStudent extends Student 
+{
+    private int numCourses;
+
+    public PartTimeStudent(int numCourses, String name) 
+    {
+        super(name);
+        this.numCourses = numCourses;
+    }
+
+    public int getNumCourses() 
+    {
+        return numCourses;
+    }
+
+    public void setNumCourses(int numCourses) 
+    {
+        this.numCourses = numCourses;
+    }
+}
